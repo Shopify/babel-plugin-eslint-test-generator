570447f9a3a481b7b211c178cb82afbab3253426
Mina Smart <mina.smart@shopify.com>
2018-06-19 10:49:09 -0400

    Update for more recent test generator

--- test/fixtures/expect-test-file.js
+++ test/fixtures/expect-test-file.js
@@ -3,12 +3,11 @@
 'use strict';
 'I AM B';
 
-describe('lint all files', function () {
-  it('test/test-src-to-lint/some-errors.js should lint', function () {
+suite('lint all files', function () {
+  test('test/test-src-to-lint/some-errors.js should lint', function () {
     assert.ok(false, 'test/test-src-to-lint/some-errors.js should pass lint.\n1:4 - Parsing error: Unexpected token ');
   });
-  it('test/test-src-to-lint/some-warnings.js should lint', function () {
+  test('test/test-src-to-lint/some-warnings.js should lint', function () {
     assert.ok(true, 'test/test-src-to-lint/some-warnings.js should pass lint.');
   });
 });
-
